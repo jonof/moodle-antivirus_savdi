292e399c94aa861103e8cde490cd690ba6377cf4
Jonathon Fowler <fowlerj@usq.edu.au>
2021-05-11 11:21:00 +1000

    bump version for release

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020121000;          // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2021051100;          // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2019111800;          // Requires this Moodle version.
 $plugin->component = 'antivirus_savdi';   // Full name of the plugin (used for diagnostics).
